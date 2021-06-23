149cc422f704e8089d050bec30e8aef48a05a0df
Honest Aguanta <haguanta@dnamicro.com>
2021-06-24 01:58:22 +0800

    setup for google maps on android

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { View, Text} from 'react-native'
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 
 const OrderDelivery = ( { navigation }) => {
+
+  function renderMap() {
+    return (
+      <View style={{ flex: 1}}>
+        <MapView style={{ flex: 1}}>
+
+        </MapView>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text>Order Delivery</Text>
+    <View style={{flex:1}}>
+      {renderMap()}
     </View>
   )
 }
